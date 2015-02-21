e406341f7042b1ea62a52cab3e1817530d09bdb9
rodrigo <lol@lol.lol>
2015-02-21 14:00:53 -0500

    change price

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -98,7 +98,7 @@ class OrderController extends Controller
             // Create the charge on Stripe's servers - this will charge the user's card
             try {
                 $charge = Charge::create(array(
-                        "amount" => 50, // amount in cents, again
+                        "amount" => 0499, // amount in cents, again
                         "currency" => "usd",
                         "source" => $token,
                         "description" => "Shoe request")
